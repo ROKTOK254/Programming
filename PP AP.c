791b6141f2fb3a87e24a9ff750f62a9f4e5b35c5
ROKTOK254 <102028353+ROKTOK254@users.noreply.github.com>
2022-04-16 18:06:49 +0300

    Add files via upload

---
+++ PP AP.c
@@ -0,0 +1,18 @@
+// programming for arithmetic operation
+#include<stdio.h>
+
+int main()
+{
+int t=12,d=6,g;
+g=t+d;
+printf("g is %d \n",g);
+g=t-d;
+printf("g is%d \n",g);
+g=t*d;
+printf("g is%d \n",g);
+g=t/d;
+printf("g is %d \n",g);
+g=t%d;
+printf("g is %d \n",g);
+    return 0;
+}
